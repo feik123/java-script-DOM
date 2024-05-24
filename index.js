8e599bf3dfe42a8aeff780f2d8e92642bbc3001c
feik123 <feik@abv.bg>
2024-05-24 20:47:06 +0300

    event listener - mouse events
    Listen for specific events to create interactive web pages
      events: click, mouseover, mouseout

--- index.js
+++ index.js
@@ -1,43 +1,21 @@
-// --------------- EXAMPLE 2 <li> ------------
+// eventListener = Listen for specific events to create interactive web pages
+//                  events: click, mouseover, mouseout
+//                  .addEventListener(event, callback(arrow or anonymous function))
 
+const myBox = document.getElementById('myBox');
+const myButon = document.querySelector('button')
 
-//      STEP = 1 CREATE THE ELEMENT
-const newListElement = document.createElement('li')
+myButon.addEventListener('click', event => {
+    myBox.style.backgroundColor = 'red';
+    myBox.textContent = 'OUCH! 🥴'
+})
 
+myButon.addEventListener('mouseover', event => {
+    myBox.style.backgroundColor = 'yellow';
+    myBox.textContent = "Don't do it 😮"
+})
 
-//      STEP 2 = CREATE ATTRIBUTES/PROPERTIES
-newListElement.textContent = 'grapes'
-newListElement.id = 'grapes';
-newListElement.style.fontWeight = 'bold';
-newListElement.style.backgroundColor = 'lightgreen'
-
-
-// newH1.textContent = 'I am a new heading';
-// newH1.id = 'myH1';
-// newH1.style.color = 'green';
-// newH1.style.textAlign = 'center'
-
-
-//      STEP 3 = APPEND ELEMENT TO DOM
-// document.getElementById('fruits').append(newListElement);
-// document.getElementById('fruits').prepend(newListElement)
-const fruits = document.querySelectorAll('li');
-document.getElementById('fruits').insertBefore(newListElement, fruits[1])
-
-
-// document.body.append(newH1);
-// document.body.prepend(newH1);
-// document.getElementById('box1').append(newH1);
-// document.getElementById('box4').prepend(newH1)
-
-// const box2 = document.getElementById('box2')
-// document.body.insertBefore(newH1, box4)
-
-// const boxes = document.querySelectorAll('.box');
-// document.body.insertBefore(newH1, boxes[2])
-
-
-// REMOVE HTML ELEMENT
-document.getElementById('fruits').removeChild(newListElement)
-
-// document.getElementById('box1').removeChild(newH1)
+myButon.addEventListener('mouseout', event => {
+    myBox.style.backgroundColor = 'lightgreen';
+    myBox.textContent = 'Clik me 😃'
+})
