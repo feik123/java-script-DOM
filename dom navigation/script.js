ca5704542c786d5752d545314d17685418129ece
feik123 <feik@abv.bg>
2024-05-24 12:47:21 +0300

    Update script.js
    Added additional selectors

--- dom navigation/script.js
+++ dom navigation/script.js
@@ -1,4 +1,40 @@
-const elements = document.getElementById('desserts');
-const children = elements.children;
+// DOM Navigation = The process of navigating through the structure
+//                  of an HTML document using JavaScript
 
-children[1].style.backgroundColor= "yellow"
+// .firstElementChild
+// .lastElementChild
+// .nextElementChild
+// .previousElementSibling
+// .parentElement
+// .children  ------- returns HTML collection
+
+// -------------------------- .firstElementChild ----------------
+
+const ulElements = document.querySelectorAll('ul');
+
+ulElements.forEach(ulElement => {
+    const firstElement = ulElement.firstElementChild;
+    firstElement.style.backgroundColor = 'LightGreen';
+    const nextSibling = firstElement.nextElementSibling;
+    nextSibling.style.backgroundColor = 'orange'
+})
+
+ulElements.forEach(ulElement => {
+    const lastChild = ulElement.lastElementChild;
+    lastChild.style.backgroundColor = 'LightBlue'
+})
+
+const element = document.getElementById('desserts');
+const previousSibling = element.previousElementSibling;
+previousSibling.style.color = 'blue'
+
+const apple = document.getElementById('apple');
+const parent = apple.parentElement;
+parent.style.color = 'Grey'
+
+const desserts = document.getElementById('desserts');
+const children = desserts.children;
+
+Array.from(children).forEach(child => {
+    child.style.fontStyle = 'italic'
+})
