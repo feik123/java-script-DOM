587827d7f144fe34eb7eb14c1a432acd89a48596
feik123 <feik@abv.bg>
2024-05-24 17:22:14 +0300

    example 2

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-// --------------- EXAMPLE 1 <h1> ------------
+// --------------- EXAMPLE 2 <li> ------------
 
 
 //      STEP = 1 CREATE THE ELEMENT
-const newH1 = document.createElement('h1');
+const newListElement = document.createElement('li')
 
 
 //      STEP 2 = CREATE ATTRIBUTES/PROPERTIES
-newH1.textContent = 'I am a new heading';
-newH1.id = 'myH1';
-newH1.style.color = 'green';
-newH1.style.textAlign = 'center'
+newListElement.textContent = 'grapes'
+newListElement.id = 'grapes';
+newListElement.style.fontWeight = 'bold';
+newListElement.style.backgroundColor = 'lightgreen'
+
+
+// newH1.textContent = 'I am a new heading';
+// newH1.id = 'myH1';
+// newH1.style.color = 'green';
+// newH1.style.textAlign = 'center'
 
 
 //      STEP 3 = APPEND ELEMENT TO DOM
+// document.getElementById('fruits').append(newListElement);
+// document.getElementById('fruits').prepend(newListElement)
+const fruits = document.querySelectorAll('li');
+document.getElementById('fruits').insertBefore(newListElement, fruits[1])
+
+
 // document.body.append(newH1);
 // document.body.prepend(newH1);
-document.getElementById('box1').append(newH1);
+// document.getElementById('box1').append(newH1);
 // document.getElementById('box4').prepend(newH1)
 
 // const box2 = document.getElementById('box2')
@@ -26,4 +38,6 @@ document.getElementById('box1').append(newH1);
 
 
 // REMOVE HTML ELEMENT
-document.getElementById('box1').removeChild(newH1)
+document.getElementById('fruits').removeChild(newListElement)
+
+// document.getElementById('box1').removeChild(newH1)
