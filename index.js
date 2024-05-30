8925e7c62b63e459b94eb5c7b7c827f2385b1bf9
feik123 <feik@abv.bg>
2024-05-31 00:39:32 +0300

    classList property
    Used to interact with an element's list of classes,
    Methods - add(), remove(), toggle(), replace(oldClass, newClass), contains(), 
    (button.classlist.add(disabled))

--- index.js
+++ index.js
@@ -1,54 +1,38 @@
-// NodeList = Static collection of HTML elements by (id, class, element)
-//              Can be created by using querySelectorAll()
-//              Similar to an array - forEach (but no: map, filter, reduce)
-//              NodeList won't update to automatically reflect changes to the DOM
+// classList = Element property in JavaScript used to interact
+//              with an element's list of classes (CSS Classes)
+//              Allows you to make reusable classes for any elements
+//              across your website
 
+//  add()
+//  remove()
+//  toggle() = Remove if present, Add if not
+//  replace(oldClass, newClass)
+//  contains()
 
 let buttons = document.querySelectorAll('.myButtons');
 
-// ADD HTML/CSS PROPERTIES
+buttons.forEach(button => {
+    button.classList.add('enabled');
+});
 
 buttons.forEach(button => {
     button.addEventListener('mouseover', event => {
-        event.target.style.backgroundColor = ' hsla(240, 71%, 40%, 0.6)'
-    });
-
-    button.addEventListener('mouseout', event => {
-        event.target.style.backgroundColor = ' hsla(240, 71%, 60%, 0.6)'
+        event.target.classList.toggle('hover')
     })
 });
 
-
-// ADD AN ELEMENT
-
-//  STEP 1 - CREATE ELEMENT
-
-const newButton = document.createElement('button');
-
-
-// STEP 2 - ADD NECESSARY ATTRIBUTES OR PROPERTIES
-
-newButton.textContent = 'Button 5';
-newButton.classList = 'myButtons';
-
-
-// STEP 3 - APPEND THE NEW ELEMENT TO THE DOM
-
-document.body.appendChild(newButton);
-
-
-buttons = document.querySelectorAll('.myButtons');
-console.log(buttons);
-
-
-// REMOVE AN ELEMENT
+buttons.forEach(button => {
+    button.addEventListener('mouseout', event => {
+        event.target.classList.toggle('hover')
+    })
+});
 
 buttons.forEach(button => {
     button.addEventListener('click', event => {
-        event.target.remove();
-
-        buttons = document.querySelectorAll('.myButtons')
-        console.log(buttons);
+        if (event.target.classList.contains('disabled')){
+            event.target.textContent += '🤬'
+        } else{
+            event.target.classList.replace('enabled', 'disabled')
+        }
     })
-
 })
