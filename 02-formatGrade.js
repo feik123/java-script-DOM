643b502604c334203999ae92936de6099a41ccdb
feik123 <feik@abv.bg>
2024-06-16 20:56:45 +0300

    calling a nested function

---
+++ 02-formatGrade.js
@@ -0,0 +1,25 @@
+function solve(grade){
+    const result = formatGrade(grade);
+
+    console.log(result);
+
+    function formatGrade(grade){
+        if (grade < 3){
+            return `Fail (2)`
+        };
+        if (grade < 3.5){
+            return `Poor (${grade.toFixed(2)})`;
+        };
+        if (grade < 4.5){
+            return `Good (${grade.toFixed(2)})`
+        }
+        if (grade < 5.5){
+            return `Very good (${grade.toFixed(2)})`
+        }
+        if (grade >= 5.5){
+            return `Excellent (${grade.toFixed(2)})`
+        }
+    }
+}
+
+solve(3.58)
